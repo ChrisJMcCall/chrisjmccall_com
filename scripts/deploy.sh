74102c0d06ba8e522e1425489f3df396c35e8092
Chris McCall <chrisjmccall@gmail.com>
2019-09-06 12:15:03 -0500

    Travisci deploy to server.

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa_chrisjmccall # Allow read access to the private key
+ssh-add .travis/id_rsa_chrisjmccall # Add the private key to SSH
+
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/build $sshuser@$targethost:$targetpath
